247c2ad9eded1563aeadb5d64ce7691be24f7e26
Pavel Savvateev <savvateev@icloud.com>
2025-03-15 10:37:12 +0800

    and corutines #1

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val authorAvatar: String,
+    val content: String,
+    val published: String,
+    val likedByMe: Boolean,
+    var likes: Int = 0,
+)
+
